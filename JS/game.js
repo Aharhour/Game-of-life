e1573d749ecff71ff83c1637394e14d26369e9fb
Aidari D. <aidari1324@gmail.com>
2024-09-03 13:59:48 +0200

    added some new functionality to the game

--- JS/game.js
+++ JS/game.js
@@ -57,7 +57,7 @@ document.addEventListener('DOMContentLoaded', () => {
         // Updates the score by adding the count of active cells to the current score
         function calculateInitialScore() {
             let initialLiveCount = cells.filter(cell => cell.classList.contains('active')).length;
-            updateScore(score + initialLiveCount);
+            updateScore(score + (initialLiveCount * 1000));
         }
 
 
@@ -129,7 +129,7 @@ document.addEventListener('DOMContentLoaded', () => {
         
             // If any cell state has changed, update the score
             if (hasChanged) {
-                updateScore(score + 1);
+                updateScore(score + 1000);
             }
         
             // Save the current state of the grid
